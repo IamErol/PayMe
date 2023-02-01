e43f24b871a0e45ee0cddb99bc49bc149c113bb0
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-02 02:38:38 +0500

    api

--- process/views.py
+++ process/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def getRoutes(request):
+    
+    routes = [
+        {'GET': 'api/urls'}
+    ]
+    
+    return Response(routes)
 
 def index(request):
     return HttpResponse('Hello')

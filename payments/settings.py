0855d26ea6adbdb998ae1574163c98f3355277d7
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-02 18:04:49 +0500

    cors

--- payments/settings.py
+++ payments/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'process',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,3 +145,7 @@ PAYME_SETTINGS = {
         'KEY_2':'',
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+]

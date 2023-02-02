af237205d783c4f2ae692cf327aa994701e6d363
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-03 04:14:45 +0500

    fullapi

--- process/serializers.py
+++ process/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from rest_framework import serializers
 
-# class SubscribeSerializer(serializers.Serializer):
-#     ...
-#     # id = serializers.IntegerField()
-#     params = serializers.IntegerField()
+
+# .data serialized data
+
+class SubscribeSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    params = serializers.JSONField(read_only=True)  #validates if data in json format
--- process/views.py
+++ process/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-# from .serializers import SubscribeSerializer
+from .serializers import SubscribeSerializer
 from payments.settings import PAYME_SETTINGS
 import requests
 from rest_framework.parsers import JSONParser 
@@ -20,59 +20,103 @@ AUTHORIZATION = {'X-Auth': '{}:{}'.format(PAYME_SETTINGS['PAY_ME_ID'], PAYME_SET
 URL = 'https://checkout.test.paycom.uz/api'
 
 
+
+
+
 class CardsCheck(APIView):
     '''Проверяем токен пластиковокй карты от фронта'''
     
     def post(self, request):
-        message = request.data.get("token")
-        return Response({'token': message})
-        # serializer = SubscribeSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     name = serializer.data
+        serializer = SubscribeSerializer(data=request.data, many=False) #data = dict object from request
+        serializer.is_valid()
+        token = serializer.validated_data["params"]["token"]  # after decoding from json we get validated data. Validated data returns a python dictionary.
+        result = self.cards_check(token)
+        return Response(result)
 
+
+    def cards_check(self, token):
+
+        data = {
+                    "id": 123,
+                    "method": "cards.check",
+                    "params": {
+                        "token": token
+                    }
+        }
+        response = requests.post(URL, json=data)
+        result = response.json()
+
+# class CardsCheck(APIView):
+#     '''Проверяем токен пластиковокй карты от фронта'''
     
-    # def post(self, request):
-    #     serializer = SubscribeSerializer(data=request.data, many=False)
-    #     serializer.is_valid(raise_exception=True)
-    #     result = self.cards_check(serializer.validated_data)
-        
-    #     return Response(result)
+#     def post(self, request):
+#         serializer = SubscribeSerializer(data=request.data, many=False) #data = dict object from request
+#         serializer.is_valid()
+#         token = serializer.validated_data['params']['token']  # after decoding from json we get validated data. Validated data returns a python dictionary.
+#         return Response(token)
     
-    # def cards_check(self, validated_data):
-    #     data = dict(
-    #         # id=validated_data['id'],
-    #         id=123456789,
-    #         method='cards.verify',
-    #         params={
-    #             'token': 'tokenvalue'
-    #         }
-    #     )
-        
-    #     response = requests.post(URL, json=data, headers=AUTHORIZATION)
-    #     result = response.json()
-    #     if 'error' in result:
-    #         return result
-        
-    #     return result
+    
+#     def card_check(self, token)
+#     # def post(self, request):
+#     #     message = request.data.get("token")
+#     #     return Response({'token': message})
+
+# class CardsRemove(APIView):
+#     '''Удаляем токен пластиковокй карты от фронта'''
+    
+#     def delete(self, request):
+#         message = request.data.get("token")
+#         return Response({'token': message})
+    
+    
+# class ReceiptsCreate(APIView):
+#     '''Создание чека на оплату'''
+    
+#     def delete(self, request):
+#         message = request.data.get("token")
+#         return Response({'token': message})
+    
 
 
-@api_view(['GET', 'POST'])
-def main(request):
-    return HttpResponse('main')
-    # if request.method == 'POST':
-    #     return Response({"message": "Got some data!", "data": request.data})
-    # return Response({"message": "Hello, world!"})
 
 
-def index(request):
-    return HttpResponse('Index')
+# def index(request):
+#     return HttpResponse('Index')
 
-# @api_view('POST')
-# def ab(request):
-#     serializer = SubscribeSerializer(data=request.data)
-#     if serializer.is_valid():
-#         name = serializer.data.get('token')
-#         message = f'token {name}'
-#         return Response({'message': message})
-#     else:
-#         return Response(serializer.errors)
+
+# class PaymentApiView(APIView):
+
+#     def post(self, request):
+#         serializer = SubscribeSerializer(data=request.data, many=False) #data = dict object from request
+#         serializer.is_valid()
+#         token = serializer.validated_data['params']['token']  # after decoding from json we get validated data. Validated data returns a python dictionary.
+#         result = self.receipts_create(token, serializer.validated_data)
+#         return Response(result)
+    
+
+#     def receipts_create(self, token, validated_data):
+#         key_2 = validated_data['params']['account'][KEY_2] if KEY_2 else None
+#         data = dict(
+#             id=validated_data['id'],
+#             method='receipts.create',
+#             params=dict(
+#                 amount=validated_data['params']['amount'],
+#                 account=validated_data['params']['account'],
+#         ))
+#         response = requests.post(URL, json=data, headers=AUTHORIZATION)
+#         result = response.json()
+#         if 'error' in result:
+#             return result
+
+
+#     def receipts_pay(self, trans_id, token):
+#         data = dict(
+#             method='receipts.pay',
+#             params=dict(
+#                 id=trans_id,
+#                 token=token,
+#             )
+#         )
+#         response = requests.post(URL, json=data, headers=AUTHORIZATION)
+#         result = response.json()
+#         return result

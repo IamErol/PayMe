3229a6a5305676af7de214b30aa3d96781bc30aa
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-02 14:50:03 +0500

    restfunctions

--- process/views.py
+++ process/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from django.http import JsonResponse
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# # from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 
-# @api_view(['GET', 'POST'])
-# def hello_world(request):
-#     if request.method == 'POST':
-#         return Response({"message": "Got some data!", "data": request.data})
-#     return Response({"message": "Hello, world!"})
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, world!"})
 
 def index(request):
     return HttpResponse('Hello')

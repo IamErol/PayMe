6a301658615416a5f029b51167761c38c6873b6c
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-02 19:54:41 +0500

    request

--- process/serializers.py
+++ process/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class SubscribeSerializer(serializers.Serializer):
     # id = serializers.IntegerField()
-    params = serializers.JSONField()
+    # params = serializers.JSONField()
--- process/urls.py
+++ process/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('main/', views.main),
-    path('api/token', views.CardsCheck.as_view()),
+    # path('api/token', views.CardsCheck.as_view()),
     path('api/token', views.ab),
 ]

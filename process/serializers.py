0328419c34a1a1fd1c4f6d628f9c4dc92989a98a
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-03 05:17:23 +0500

    api

--- process/serializers.py
+++ process/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 
 class SubscribeSerializer(serializers.Serializer):
     # id = serializers.IntegerField(read_only=True)
-    info = serializers.JSONField(read_only=True)  #validates if data in json format
+    info = serializers.JSONField()  #validates if data in json format

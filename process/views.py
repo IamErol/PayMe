2d38fd03cca2158e159a196c1356c44a45780e5e
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-04 22:37:11 +0500

    supabase_uninstalled

--- process/views.py
+++ process/views.py
@@ -12,7 +12,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 
 import os
-from supabase.client import create_client, Client
+# from supabase.client import create_client, Client
 from dotenv import load_dotenv
 from random import randint
 load_dotenv()

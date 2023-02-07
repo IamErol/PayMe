884c1b4f4e32ce70f186dbfd96db5787d457c445
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-08 02:02:14 +0500

    prod

--- process/urls.py
+++ process/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('main/', views.main),
     path('cards/create/', views.CardsCreate.as_view()),
     path('api/verify/', views.CardVerify.as_view(), name='card_verify'),
-    path('api/receipts/', views.Receipts.as_view()),
+    # path('api/receipts/', views.Receipts.as_view()),
     # path('api/cards/verify', views.CardsVerify.as_view()),
     # path('api/cards/remove', views.CardsRemove.as_view()),
     # path('api/receipts', views.Receipts.as_view()),

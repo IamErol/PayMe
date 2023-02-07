3ffda68a0693c24b25b749f8b2d9af630a7d1028
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-07 14:51:30 +0500

    check_api

--- process/urls.py
+++ process/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('main/', views.main),
-    # path('api/cards/check', views.CardsCheck.as_view()),
+    path('api/cards/check', views.CardsCheck.as_view()),
     # path('api/cards/remove', views.CardsRemove.as_view()),
     # path('api/receipts', views.Receipts.as_view()),
     path('api/cards/create', views.CardsCreate.as_view()),

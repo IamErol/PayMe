944310496e2d9d2247e5d2065712d8210eb3e6a2
IamErol <yildirimerol7.0.0@gmail.com>
2023-02-12 02:00:06 +0500

    supa

--- process/views.py
+++ process/views.py
@@ -177,16 +177,13 @@ class CardVerify(APIView):
             result.update(fail='pay', token=token, receipts_pay_response=result)
             return result
         
-        if result['result']['receipt']['state'] == 4:
-            validated_data.update(transaction_order_id=transaction_order_id)
-            try:
-                TRANSACTION = sup.transactions_data_to_insert(result, validated_data)
-                ORDERS = sup.orders_data_to_insert(result, validated_data)
-                sup.insert_data(TRANSACTION, 'transactions')
-                sup.insert_data(ORDERS, 'orders')
-                result.update(data_is_saved='True')
-            except:
-                result.update(data_is_saved='False')
+
+        TRANSACTION = sup.transactions_data_to_insert(result, validated_data)
+        ORDERS = sup.orders_data_to_insert(result, validated_data)
+        sup.insert_data(TRANSACTION, 'transactions')
+        sup.insert_data(ORDERS, 'orders')
+        result.update(data_is_saved='True')
+
             
 
         result.update(status='pay success')
